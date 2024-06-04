b7b9babfcc4845d0f4a51271834870271d9a2483
li <li@maisiliym.kriom.me>
2024-06-04 16:43:58 -0600

    (massRename)

--- datom.nix
+++ datom.nix
@@ -8,7 +8,7 @@
         modyl = "rock64";
         korz = 4;
       };
-      priKriomz = {
+      preCriomes = {
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIPvxPTAPPKe6Ne9MW4C04Ihe0g4NS12xydci/JW59pIe";
       };
       io = {
@@ -48,11 +48,11 @@
         };
         swapDevices = [{ device = "/dev/disk/by-uuid/17a56c75-f8fc-428c-b8a4-df3d9449fcf7"; }];
       };
-      priKriomz = {
-        niksPriKriom = "0J1372hbJdemn5rVicpVSOVjB2cm8uyI3ypkFoRihRM=";
+      preCriomes = {
+        niksPreCriome = "0J1372hbJdemn5rVicpVSOVjB2cm8uyI3ypkFoRihRM=";
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIOQascm6i8E72qmmcaw114fSStgy3syEnNp3hv2qJvbr";
         yggdrasil = {
-          priKriom = "e48fd5a04f1c2fd1f73707dd4dcc1e7e1513c5575b4eb585bffd38b347a8dbf1";
+          preCriome = "e48fd5a04f1c2fd1f73707dd4dcc1e7e1513c5575b4eb585bffd38b347a8dbf1";
           address = "200:36e0:54bf:61c7:a05c:1191:f045:6467";
           subnet = "300:36e0:54bf:61c7";
         };
@@ -86,11 +86,11 @@
         };
         swapDevices = [{ device = "/dev/disk/by-uuid/4dcf2ad2-4677-483f-9900-046f0f53053a"; }];
       };
-      priKriomz = {
-        niksPriKriom = "P2pDdZrmbLO8EBUuwLvt+FJlqh4ZRVczgGV/Tmr1Bis=";
+      preCriomes = {
+        niksPreCriome = "P2pDdZrmbLO8EBUuwLvt+FJlqh4ZRVczgGV/Tmr1Bis=";
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIKeoCIfvbUDue17FBglmKF1N2cKH9CWsS5EcXOyBxq5T";
         yggdrasil = {
-          priKriom = "38ed4c7c9ffd2e79361af3202f406810069fa37ff991e6d1d434a529ba75dfb3";
+          preCriome = "38ed4c7c9ffd2e79361af3202f406810069fa37ff991e6d1d434a529ba75dfb3";
           address = "202:3895:9c1b:16:8c36:4f28:66fe:85fc";
           subnet = "302:3895:9c1b:16";
         };
@@ -110,7 +110,7 @@
 
       neksysIp = "5::1/128";
       linkLocalIPs = [{ spici = "ethernet"; suffix = "aec6:ecad:34e0:b41f"; }];
-      wireguardPriKriom = "oOeqiGZC+oaIL0rKirFyiJ4N6qDQWICZ2wRelLMOR0o=";
+      wireguardPreCriome = "oOeqiGZC+oaIL0rKirFyiJ4N6qDQWICZ2wRelLMOR0o=";
 
       io = {
         kibord = "colemak";
@@ -145,11 +145,11 @@
           { device = "/dev/disk/by-uuid/03a014a2-9d6c-4c8e-b4be-b01415ad1beb"; }
         ];
       };
-      priKriomz = {
+      preCriomes = {
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIA0GR4qcTTTgkdes3aYFz998tWC6w0wss4nRDfvAFBda";
-        niksPriKriom = "AAqjPi1kOaOWyZrwupTpHNL/7SYjYmaeDu1J+hNtVFA=";
+        niksPreCriome = "AAqjPi1kOaOWyZrwupTpHNL/7SYjYmaeDu1J+hNtVFA=";
         yggdrasil = {
-          priKriom = "2748bfce76cf7c2a8d6d73241fadd4cad2a32f631df6a4be4bfb33969b0490e0";
+          preCriome = "2748bfce76cf7c2a8d6d73241fadd4cad2a32f631df6a4be4bfb33969b0490e0";
           address = "202:c5ba:18c:4984:1eab:9494:66df:291";
           subnet = "302:c5ba:18c:4984";
         };
@@ -168,7 +168,7 @@
       };
       neksysIp = "5::2/128";
       linkLocalIPs = [{ spici = "ethernet"; suffix = "3e97:eff:fea3:30d2"; }];
-      wireguardPriKriom = "3HeLSQr+lbJp4w9oUILEVWrBpYYCoQzxkARWDCBf5j8=";
+      wireguardPreCriome = "3HeLSQr+lbJp4w9oUILEVWrBpYYCoQzxkARWDCBf5j8=";
       io = {
         kibord = "colemak";
         butlodyr = "uefi";
@@ -184,14 +184,14 @@
         };
         swapDevices = [ ];
       };
-      priKriomz = {
+      preCriomes = {
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIDjXtLUY5gSvogSTzU54x/EryOgNVZ2HkCYQL1d9lKEB";
         yggdrasil = {
-          priKriom = "c9eca45c87e2f4478eec5488e2183fcf5536a34352201dec785c74dd1946268c";
+          preCriome = "c9eca45c87e2f4478eec5488e2183fcf5536a34352201dec785c74dd1946268c";
           address = "200:6c26:b746:f03a:1770:e227:56ee:3bcf";
           subnet = "300:6c26:b746:f03a";
         };
-        niksPriKriom = "cV4q5ycwjEd/VhSKhZolX8VVhv47MHSfatOBNq7bjuE=";
+        niksPreCriome = "cV4q5ycwjEd/VhSKhZolX8VVhv47MHSfatOBNq7bjuE=";
       };
     };
 
@@ -224,14 +224,14 @@
         };
         swapDevices = [ ];
       };
-      priKriomz = {
+      preCriomes = {
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAICWYATms2fd9JVJNrMetB8uNNP/ZKNG9/QhJBkyJ/6VX";
         yggdrasil = {
-          priKriom = "d7cbcd3122a0ba4d0ca1f662ecbab4bc376343ece193749c3f94a6e1b24f42db";
+          preCriome = "d7cbcd3122a0ba4d0ca1f662ecbab4bc376343ece193749c3f94a6e1b24f42db";
           address = "200:5068:659d:babe:8b65:e6bc:133a:268a";
           subnet = "300:5068:659d:babe";
         };
-        niksPriKriom = "Mx+wLIfTWjYm2b+gI6X5vxY0eGtScNB1+51DxIZbeus=";
+        niksPreCriome = "Mx+wLIfTWjYm2b+gI6X5vxY0eGtScNB1+51DxIZbeus=";
       };
     };
 
@@ -260,11 +260,11 @@
         };
         swapDevices = [{ device = "/dev/disk/by-uuid/f86fc799-cbc2-473b-94ce-abfc1aa040f2"; }];
       };
-      priKriomz = {
+      preCriomes = {
         eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAID7E6ca/f2Bkz5Z9ZX4ASHXPLF7TYI/Z4C0/Is/Ljpyu";
-        niksPriKriom = "7N3PTqwWdA0p4XNjupKhLhVD5D0cEFcY/mUk3FRo5lI=";
+        niksPreCriome = "7N3PTqwWdA0p4XNjupKhLhVD5D0cEFcY/mUk3FRo5lI=";
         yggdrasil = {
-          priKriom = "cef05ea020845caf73f2b3b3c15d27e3d55dd35b5155692a5464b7920b6b03b9";
+          preCriome = "cef05ea020845caf73f2b3b3c15d27e3d55dd35b5155692a5464b7920b6b03b9";
           address = "200:621f:42bf:bef7:46a1:181a:9898:7d45";
           subnet = "300:621f:42bf:bef7";
         };
@@ -278,19 +278,19 @@
       mycin = {
         spici = "pod";
         ubyrAstri = "xerxes";
-        ubyrKrimyn = "li";
+        ubyrUser = "li";
         korz = 1;
       };
     };
   };
 
-  krimynz = {
+  users = {
     li = {
       spici = "Onlimityd";
       saiz = 3;
       kibord = "colemak";
       githubId = "LiGoldragon";
-      priKriomz = {
+      preCriomes = {
         dante = {
           eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIK769R2iKyr5rgBvR9OFeSN2kdo8h+LtXVUjzdFLf4vl";
           keygrip = "AD305831DD33E62F9AD587718D4E5E6999CD84EA";
@@ -310,7 +310,7 @@
       spici = "Sema";
       saiz = 3;
       kibord = "colemak";
-      priKriomz = {
+      preCriomes = {
         klio = {
           eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIKTc1b3BgAHoY1R7gY8YsaPI7AeTZzDyWJPhlyEkjv/e";
           keygrip = "2B02ECA0AE0D9A027C013234DEB7888B37E70DBF";
@@ -322,7 +322,7 @@
       spici = "Niks";
       saiz = 1;
       kibord = "colemak";
-      priKriomz = {
+      preCriomes = {
         xerxes = {
           eseseitc = "AAAAC3NzaC1lZDI1NTE5AAAAIEygb1Ft1hIB+ExPGLq08im9rFYvOeYXX+NetgqjI3Db";
           keygrip = "63A149ECC539BAF2B001C57E9A55A566BCD48446";
@@ -345,7 +345,7 @@
       juno = 2;
       tiger = 3;
     };
-    krimynz = {
+    users = {
       maikro = 3;
       li = 3;
       bird = 2;
